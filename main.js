941e173b6f82dd8b54874e19535eb2c41446ded3
etneuhct <jacktchuente@gmail.com>
2020-04-05 12:58:34 -0400

    ajout comm., corr.

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function getBlockedUserMessage(userName, storage) {
-	var messagesToDelete = document.querySelectorAll('div[data-pre-plain-text*="' + userName + '"')	
+	const messagesToDelete = document.querySelectorAll('div[data-pre-plain-text*="' + userName + '"');
 	for (const messageToDelete of messagesToDelete) {
-		var messageToDeleteId = messageToDelete.parentElement.parentElement.parentElement.getAttribute("data-id")
+		const messageToDeleteId = messageToDelete.parentElement.parentElement.parentElement.getAttribute("data-id");
 		storage.push(messageToDeleteId)
 	}
 }
@@ -13,32 +13,36 @@ function simulateMouseEvents(element, eventName) {
 }
 
 function main(blockedUsers) {
-	var messagesIds = []
+	var messagesIds = [];
 	
 	for (const blockedUser of blockedUsers) {
 		getBlockedUserMessage(blockedUser, messagesIds)
 	}
 
 	if (messagesIds.length > 0) {
-		var mainMenu = document.querySelector('div#main div[title="Menu"][role="button"]')
-		simulateMouseEvents(mainMenu, 'mousedown')
-		mainMenu.parentElement.querySelectorAll('span>div>ul>li')[1].click()
+		const mainMenu = document.querySelector('div#main div[title="Menu"][role="button"]');
+		simulateMouseEvents(mainMenu, 'mousedown');
+		mainMenu.parentElement.querySelectorAll('span>div>ul>li')[1].click();
 		for (const messageId of messagesIds) {
-			let messageDiv = document.querySelector('div[data-id="'+ messageId +'"]')
-			let messageButton = messageDiv.parentElement.querySelector("span>div")
+			let messageDiv = document.querySelector('div[data-id="'+ messageId +'"]');
+			let messageButton = messageDiv.parentElement.querySelector("span>div");
 			messageButton.click()
 		}
-		let deleteBtn = document.querySelector("span[data-icon='delete'")
-		deleteBtn.click()
-		let confirmDelete = document.querySelectorAll("div[data-animate-modal-body='true'] div[role='button'")[1]
-		if (confirmDelete.textContent == "Supprimer pour moi") {
+		let deleteBtn = document.querySelector("span[data-icon='delete'");
+		deleteBtn.click();
+		let confirmDelete = document.querySelectorAll("div[data-animate-modal-body='true'] div[role='button'")[1];
+		if (confirmDelete.textContent === "Supprimer pour moi") {
 			confirmDelete.click()
 		}
 	}
 }
+// liste de Numero de l'utilisateur tel qu'affiché par WA
+// si l'utilisateur n'est pas enr. sur le tel.
+// ou Nom de l'utilisateur tel qu'enregistré sur le telephone
 
-var blockedUsers = ["Bamba"]
-var intervalMS = 1000
+const blockedUsers = [""];
+
+const intervalMS = 1000;
 setInterval(function() {
 	main(blockedUsers)
-}, intervalMS)
+}, intervalMS);
